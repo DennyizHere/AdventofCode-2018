a32c6020fd4710f5fc41613c653d88205c7d8797
Unknown <dennycao202@yahoo.com>
2019-10-28 17:56:54 -0700

    Day 1, Part 2 done

--- Day-1/src/Day1.java
+++ Day-1/src/Day1.java
@@ -13,7 +13,8 @@ public class Day1 {
 			action = stuff.nextLine();
 		}
 		
-		System.out.println(part1(numbers));
+		System.out.println("Part 1 answer: " + part1(numbers));
+		System.out.println("Part 2 answer: " + part2(numbers));
 	}
 	
 	static int part1(List<Integer> moves) {
@@ -21,6 +22,25 @@ public class Day1 {
 		
 		for (int i = 0; i < moves.size(); i++) {
 			total += moves.get(i);
+		}
+		
+		return total;
+	}
+	
+	static int part2(List<Integer> moves) {
+		HashSet<Integer> freqs = new HashSet<Integer>();
+		int total = 0;
+		freqs.add(0);
+		
+		for (int i = 0; i < moves.size(); i++) {
+			total += moves.get(i);
+			if (freqs.contains(total)) {
+				break;
+			}
+			else {
+				freqs.add(total);
+			}
+			if (i == moves.size() - 1) i = -1;
 		}
 		
 		return total;
